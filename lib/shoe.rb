cff4726fb9f2ca37096a0848cebf0277eb4b2f30
Lillian Byrne <lily.byrne.304@gmail.com>
2018-06-27 02:19:41 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+
+class Shoe 
+  attr_accessor :color, :size, :material
+  attr_reader :brand, :condition 
+  
+  def initialize (brand)
+    @brand = brand
+  end 
+  
+  def initialize (condition)
+    @condition = condition 
+  end 
+  
+  def cobble
+    puts "Has the shoe been repaired?"
+  end 
+  
+end 
+
+  
---
+++ notes.rb
@@ -0,0 +1,14 @@
+#object oriented. the purpose of a class is to create objects. when something has a bunch of different attributes you have to use a class. 
+# Class >  instance > properties(attributes), use @ for attributes > behaviors (methods)
+#Classese must be capitalized. 
+#defining a class 
+
+class College
+  def initialize
+    @student_population
+    @mascot
+    @type 
+  end 
+end 
+
+#making a new class > class college.new 
